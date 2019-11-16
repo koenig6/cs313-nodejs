45d8910f5c324d646b334d42305cb7bfb75ad5ce
Jody <koenig6@byui.edu>
2019-11-16 09:24:45 -0500

    remove excess

--- indexRate.js
+++ indexRate.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl:true
-});
 
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
@@ -16,22 +11,7 @@ const pool = new Pool({
             res.render('pages/index');
           });
   app.get('/postalRateCalc', calculateRate);
-  app.get('db', async function(req, res)
-       {
-            try
-            {
-                const client = await pool.connect();
-                const result = await client.query('SELECT * FROM test_table');
-                const results = {'results': (result) ? result.rows : null};
-                res.render('pages/db', results);
-                client.release();
-            }//end of try
-            catch(err)
-                {
-                    console.error(err);
-                    res.send("Error " + err);
-                }//end catch
-        });
+
   app.listen(PORT, function()
              {
                 console.log(`Listening on`, PORT);
